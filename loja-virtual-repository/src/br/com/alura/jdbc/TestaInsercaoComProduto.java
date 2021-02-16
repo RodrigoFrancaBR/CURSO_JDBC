f71ccfbe86671f2978a59dceec912cb0edd06efc
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-02-15 22:40:01 -0200

    Trabalhando com DAO

--- loja-virtual-repository/src/br/com/alura/jdbc/TestaInsercaoComProduto.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/TestaInsercaoComProduto.java
@@ -1,31 +1,24 @@
 package br.com.alura.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 
+import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.modelo.Produto;
 
 public class TestaInsercaoComProduto {
 	public static void main(String[] args) throws SQLException {
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
-
-		try (Connection c = new ConnectionFactory().recuperarConexao()) {
-			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES(?,?)";
-			try (PreparedStatement pstm = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				pstm.setString(1, comoda.getNome());
-				pstm.setString(2, comoda.getDescricao());
-				pstm.execute();
-				try (ResultSet rs = pstm.getGeneratedKeys()) {
-					while (rs.next()) {
-						comoda.setId(rs.getInt(1));
-					}
-				}
-
-			}
+		
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
+			produtoDAO.salvar(comoda);
+						
+			List<Produto>listaDeProdutos = produtoDAO.listar();
+			
+			listaDeProdutos.stream().forEach(System.out::println);
 		}
-		System.out.println(comoda);
+		
 	}
 }
---
+++ loja-virtual-repository/src/br/com/alura/jdbc/dao/ProdutoDAO.java
@@ -0,0 +1,52 @@
+package br.com.alura.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.jdbc.modelo.Produto;
+
+public class ProdutoDAO {
+	private Connection connection;
+	private final String SELECT = "SELECT ID, NOME, DESCRICAO FROM PRODUTO";
+	private final String INSERT = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES(?,?)";
+
+	public ProdutoDAO(Connection connection) {
+		this.connection = connection;
+	}
+
+	public void salvar(Produto produto) throws SQLException {
+
+		try (PreparedStatement pstm = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS)) {
+			pstm.setString(1, produto.getNome());
+			pstm.setString(2, produto.getDescricao());
+			pstm.execute();
+			try (ResultSet rs = pstm.getGeneratedKeys()) {
+				while (rs.next()) {
+					produto.setId(rs.getInt(1));
+				}
+			}
+
+		}
+	}
+
+	public List<Produto> listar() throws SQLException {
+		Produto p;
+		List<Produto> listaDeProdutos = new ArrayList<>();
+		try (PreparedStatement pstm = connection.prepareStatement(SELECT, Statement.RETURN_GENERATED_KEYS)) {
+			pstm.execute();
+			try (ResultSet rs = pstm.getResultSet()) {
+				while (rs.next()) {
+					p = new Produto(rs.getInt(1), rs.getString(2), rs.getString(3));
+					listaDeProdutos.add(p);
+				}
+			}
+		}
+		return listaDeProdutos;
+	}
+
+}
